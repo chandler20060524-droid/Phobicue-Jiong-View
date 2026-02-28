76ad2e9829660d0996e9e12d614adf51d07ae923
Chandler Qian <chandler20060524@gmail.com>
2026-02-28 16:19:59 +0800

    Implemented and testing prototype object detection and bluring based on animals; need more learning and information.

--- src/Phobicue.py
+++ src/Phobicue.py
@@ -4,7 +4,25 @@ import win32gui, win32ui, win32con, win32api
 from pynput import mouse, keyboard
 from ctypes import windll
 import threading 
+from ultralytics import YOLO
 
+# Load image classification model
+model = YOLO("yolov8n.pt")
+
+ANIMAL_CLASSES = {
+    15,  # cat
+    16,  # dog
+    17,  # horse
+    18,  # sheep
+    19,  # cow
+    20,  # elephant
+    21,  # bear
+    22,  # zebra
+    23,  # giraffe
+    14,  # bird
+}
+
+# Mouse and keyboard events tracking. 
 class InputState:
     def __init__(self):
         self.lock = threading.Lock()
@@ -39,27 +57,34 @@ def on_scroll(x, y, dx, dy):
 mouse.Listener(on_move=on_move, on_click=on_click, on_scroll=on_scroll).start()
 keyboard.Listener(on_press=on_key).start()
 
-
+# Window title to capture
 window_title = "Google Chrome"
 
+# Create cv2 window
 cv2.namedWindow("Phobicue", cv2.WINDOW_NORMAL)
 
+# Get window information
 windll.user32.SetProcessDPIAware()
 hwnd = win32gui.FindWindow(None, window_title)
 left, top, right, bottom = win32gui.GetClientRect(hwnd)
 width = right - left
 height = bottom - top
 
+# Get window handle from target window
 hdc = win32gui.GetWindowDC(hwnd)
 mfcDC  = win32ui.CreateDCFromHandle(hdc)
 saveDC = mfcDC.CreateCompatibleDC()
 
+# Get bitmap as image input buffer
 saveBitMap = win32ui.CreateBitmap()
 saveBitMap.CreateCompatibleBitmap(mfcDC, width, height)
 
 def main():
+    frame_count = 0
+    safety_counter = 30
+    previous_detections = []
     while state.running:
-        try:    
+        try:   
             saveDC.SelectObject(saveBitMap)
             result = windll.user32.PrintWindow(hwnd, saveDC.GetSafeHdc(), 2)
             if result != 1:
@@ -70,6 +95,25 @@ def main():
             img = np.frombuffer(bmpstr, dtype=np.uint8)
             img = img.reshape((height, width, 4))
             cv_frame = cv2.cvtColor(img, cv2.COLOR_RGBA2RGB)
+
+            # reuse detection every 3 frames
+            if frame_count % 3 == 0:
+                detections = DetectAnimal(cv_frame, 0.2)
+
+            # previous detection last at least 30 frames to ensure 
+            # no flickerings
+            if len(detections) != 0:
+                previous_detections = detections.copy()
+                safety_counter = 30
+
+            for x1, y1, x2, y2, conf in detections:
+                RegionBlur(cv_frame, (x1, y1, x2, y2))
+
+            if safety_counter > 0 and previous_detections:
+                for x1, y1, x2, y2, conf in previous_detections:
+                    RegionBlur(cv_frame, (x1, y1, x2, y2))
+
+                safety_counter -= 1
 
             win_x, win_y, win_width, win_height = cv2.getWindowImageRect("Phobicue")
 
@@ -83,7 +127,7 @@ def main():
             y = int((abs_y - win_y) / win_height * height)
 
             x -= 7
-            y -= 150
+            y -= 200
 
             if left_pressed:
                 click(x, y, 0)
@@ -95,6 +139,8 @@ def main():
                 # scroll(x, y, scrolled * 120)
 
             cv2.imshow("Phobicue", cv_frame)
+
+            frame_count += 1
 
             if cv2.waitKey(25) & 0xFF == 27:
                 break
@@ -127,6 +173,10 @@ def click(x, y, input):
         print(f"Click failed: {e}")
 
 def enum_child_windows(hwnd):
+    '''
+    Enumerate through window handles to find a child window that acceots
+    mouse wheel scroll messsage (currently not implemented).
+    '''
     children = []
     def callback(child, param):
         children.append(child)
@@ -147,11 +197,44 @@ def scroll(x, y, delta):
     except Exception as e:
         print(f"Scroll failed: {e}")
 
-if __name__ == "__main__":
-    main()
+def DetectAnimal(frame, conf_threshold = 0.4):
+    '''
+    Returns a list of bounding boxex on successfully detected animals.
+    '''
+    # resized = cv2.resize(frame, (640, 640)) 
+    # faster detection since YOLO is trained on 640 x 640 images
+    results = model(frame, verbose=False)
+    detections = []
 
-    camera.stop()
-    cv2.destroyAllWindows()
+    for r in results:
+        for box in r.boxes:
+            cls = int(box.cls[0])
+            conf = float(box.conf[0])
+
+            if cls in ANIMAL_CLASSES and conf >= conf_threshold:
+                x1, y1, x2, y2 = map(int, box.xyxy[0])
+                # rescale detected position based on frame size
+                # x1 = int(x1 / 640 * width)
+                # y1 = int(y1 / 640 * height)
+                # x2 = int(x2 / 640 * width)
+                # y2 = int(y2 / 640 * height)
+                detections.append((x1, y1, x2, y2, conf))
+    
+    return detections
+
+def RegionBlur(frame, box):
+    '''
+    Blurs region based on successful detections.
+    '''
+    x1, x2, y1, y2 = box
+    roi = frame[y1:y2, x1:x2]
+
+    if roi.size == 0:
+        return 
+    
+    blurred = cv2.GaussianBlur(roi, (51, 51), 0)
+    frame[y1:y2, x1:x2] = blurred
+
 
 if __name__ == "__main__":
     main()
